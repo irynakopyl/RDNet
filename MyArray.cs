a3a5601057dbda05353c4f22015fab1115d524e7
irynakopyl <52530587+irynakopyl@users.noreply.github.com>
2019-07-04 12:55:52 +0300

    Add files via upload

---
+++ MyArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Arr
+{
+    public class MyArray<T>
+    {
+        int start;
+        int length;
+
+        T[] array;
+
+        public int Lenght { get { return array.Length; } }
+        public string Message { get { return "WRONG INDEXES!"; } }
+
+        public MyArray(int startIndex, int len)
+        {
+            this.start = startIndex;
+            length = len;
+            array = new T[len];
+        }
+
+        public T this[int index]
+        {
+            get {
+                if (index - start < 0) throw new IndexOutOfRangeException(Message);
+                return array[index - start]; }
+            set { array[index - start] = value; }
+        }
+    }
+}
+
---
+++ UnitTestMyArray.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Arr;
+
+namespace UnitTestMyArray
+{
+    [TestClass]
+    public class ArrayUnitTest
+    {
+        [TestMethod]
+        public void Indexator_5elem_125Returned()
+        {
+            //arrange
+            int firstElem = 3;
+            int amount = 6;
+            int expected = 125;
+            MyArray<int> testing = new MyArray<int>(firstElem, amount);
+            testing[5] = expected;
+            //act
+            int actual = testing[5];
+            //assert
+            Assert.AreEqual(125, actual);
+
+        }
+        [TestMethod]
+        public void Indexator_IndexBiggerThanAmount_IndexOutOfRangeExceptionExpected()
+        {
+            //arrange
+            int firstElem = 3;
+            int amount = 6;
+            MyArray<int> testing = new MyArray<int>(firstElem, amount);
+
+            //act
+            try
+            {
+                int result = testing[10];
+            }
+            catch(IndexOutOfRangeException e)
+            { StringAssert.Contains(e.Message, testing.Message);
+                return;
+            }
+            //assert
+            Assert.Fail("No exceptions");
+
+        }
+       
+    }
+}
