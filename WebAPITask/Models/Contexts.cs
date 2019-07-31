f7078134e7174b8827a53bcbfcd4c4accc85e946
irynakopyl <52530587+irynakopyl@users.noreply.github.com>
2019-07-31 22:36:41 +0300

    Add files via upload
    There is only a few files, because Git didn't allow me to download all the project, so there is only things that I've  done myself

---
+++ WebAPITask/Models/Contexts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class OrderContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
