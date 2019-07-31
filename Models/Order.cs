9d8965c21cb5061d0172a31defb1cb1d2a4dd862
irynakopyl <52530587+irynakopyl@users.noreply.github.com>
2019-07-31 23:31:55 +0300

    Add files via upload

---
+++ Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Customer { get; set; }
+        public List<Product> products { get; set; }
+    }
+}
